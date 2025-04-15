503fbbc8a5e8257721f9837330b5f6dae0ae250a
diegojgc1 <diegojgc1@localhost>
2025-04-16 00:06:30 +0200

    Cuarto Commit

--- src/prodiegojesus/Programa.java
+++ src/prodiegojesus/Programa.java
@@ -3,7 +3,7 @@ package prodiegojesus;
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Tercer Cambio con Git");
+		System.out.println("Cuarto Cambio con Git desde el primer usuario");
 	}
 
 }
