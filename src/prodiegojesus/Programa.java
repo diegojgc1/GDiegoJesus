448a5d774c2439baad842c6ee04170ae89eedd79
diegojgc1 <diegojgc1@localhost>
2025-04-15 15:58:56 +0200

    Mi Segundo Commit

--- src/prodiegojesus/Programa.java
+++ src/prodiegojesus/Programa.java
@@ -3,8 +3,7 @@ package prodiegojesus;
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Mi Primer Commit en Git");
-
+		System.out.println("Adios Git");
 	}
 
 }
