f49e52a8a76648e33e248c5db0bc5c9427742fef
diego <diego@DIEGO>
2025-04-16 00:26:04 +0200

    Cambio desde el segundo usuario

--- src/prodiegojesus/Programa.java
+++ src/prodiegojesus/Programa.java
@@ -3,7 +3,7 @@ package prodiegojesus;
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Cuarto Cambio con Git desde el primer usuario");
+		System.out.println("Cambio desde el segundo usuario");
 	}
 
 }
