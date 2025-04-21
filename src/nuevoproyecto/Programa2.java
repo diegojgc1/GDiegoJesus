82d7497a2d59af1ca7d1a2eb4ac990d555f124af
diego <diego@DIEGO>
2025-04-21 21:53:13 +0200

    Modificacion en Programa2 desde el segundo usuario

--- src/nuevoproyecto/Programa2.java
+++ src/nuevoproyecto/Programa2.java
@@ -3,7 +3,7 @@ package nuevoproyecto;
 public class Programa2 {
 
 	public static void main(String[] args) {
-		System.out.println("ESTE ES EL NUEVO PROGRAMA CREADO DESDE LA RAMA");
+		System.out.println("MODIFICACION DESDE LA RAMA");
 
 
 }
