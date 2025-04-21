2a0ce0ef4394eb3d1d2db44c9d97956488ebec1d
diegojgc1 <diegojgc1@localhost>
2025-04-21 21:00:52 +0200

    Añadido metodo saludar en el programa 2

--- src/nuevoproyecto/Programa2.java
+++ src/nuevoproyecto/Programa2.java
@@ -5,6 +5,10 @@ public class Programa2 {
 	public static void main(String[] args) {
 		System.out.println("ESTE ES EL NUEVO PROGRAMA CREADO DESDE LA RAMA");
 
-	}
 
+}
+
+	public static void saludar(String nombre) {
+	    System.out.println("Hola, " + nombre + "!");
+	}
 }
